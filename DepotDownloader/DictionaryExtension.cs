118888ae944e032dea8a1de31777d33b05d7b0d9
Gerd Katzenbeisser <gerd.katzenbeisser@gmail.com>
2022-02-17 11:33:29 +0100

    Add missing DictionaryExtension for net472

---
+++ DepotDownloader/DictionaryExtension.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DepotDownloader;
+
+public static class DictionaryExtension
+{
+    public static bool TryAdd<TKey, TValue>(this Dictionary<TKey, TValue> dictionary, TKey key, TValue value)
+    {
+        try
+        {
+            dictionary.Add(key, value);
+            return true;
+        }
+        catch (Exception e) when (e is not ArgumentNullException)
+        {
+            return false;
+        }
+    }
+}
